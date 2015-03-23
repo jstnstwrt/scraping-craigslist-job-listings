a5d4c057f907379ba0bb543ed4a602247a7c21b8
justinstewart.la <justinstewart.la@gmail.com>
2015-03-23 16:38:13 -0400

    added ability to collect the title of the post

--- cl-scrape.py
+++ cl-scrape.py
@@ -15,19 +15,27 @@ soup = BeautifulSoup(r.content)
 
 
 ## collecting all the paragraph tags in the page source
-g_data = soup.find_all("p",{"class": "row"})
+cl_data = soup.find_all("p",{"class": "row"})
 
 
-## 
+## creating a list to store the data
 cl_listings = []
 
-for item in g_data:
+
+# looping through the cl data
+for item in cl_data:
+
+	#identifying the link to the job listing
+	title_start = listing_element.find('>')+1
+	title_end = listing_element.find('<',title_start)
+	title = listing_element[title_start:title_end]
 	
+	#identifying the link to the job listing
 	listing_element = str(item.find_all('a',{"class":"hdrlnk"}))
 	link_start = listing_element.find('href="') + len('href="')
 	link_end = listing_element.find('"',link_start)
 	link = listing_element[link_start:link_end]
-	
+
 
 	cl_listings.append(base_url+link)
 
